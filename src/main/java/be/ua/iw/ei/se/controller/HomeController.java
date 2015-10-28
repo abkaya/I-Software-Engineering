c155a1c3d6244fb3069c636293d2ec142095eeed
Edwin <edwin.walsh@uantwerpen.be>
2015-10-28 22:09:33 +0100

    SE-1

--- src/main/java/be/ua/iw/ei/se/controller/HomeController.java
+++ src/main/java/be/ua/iw/ei/se/controller/HomeController.java
@@ -23,51 +23,11 @@ public class HomeController {
     @Autowired
     private UserService userService;
 
-    @ModelAttribute("allUsers")
-    public Iterable<User> populateUsers() {
-        return this.userService.findAll();
-    }
-
-    @ModelAttribute("allRoles")
-    public List<Role> populateRoles() {
-
-        List<Role> allRoles = new ArrayList<>();
-        Iterable<User> users = userService.findAll();
-        for (User user : users){
-            Iterable<Role> roles = user.getRoles();
-            for (Role role : roles){
-                if (!allRoles.contains(role)){
-                    allRoles.add(role);
-                }
-            }
-        }
-        return allRoles;
-    }
-
-
     @RequestMapping({"/","/home"})
     @PreAuthorize("hasRole('logon')")
     public String showHomepage(final ModelMap model){
         model.addAttribute("user", new User());
         return "homepage";
-    }
-
-
-    @RequestMapping(value="/users/add", method=RequestMethod.POST)
-    public String add(@ModelAttribute(value="user") User user, BindingResult result, final ModelMap model) {
-        if(result.hasErrors()){
-            return "/users/add";
-        }
-        userService.add(user);
-        model.clear();
-        return "redirect:/home";
-    }
-
-
-    @RequestMapping(value="/users/{id}/delete")
-    public String delete(@PathVariable Long id, ModelMap model) {
-        userService.delete(id);
-        return "redirect:/home";
     }
 
 

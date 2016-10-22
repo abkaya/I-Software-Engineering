221778bbb2ffb3bfaa23be73c51f94d43a4d18c7
Abdil Kaya <abdil.kaya@student.uantwerpen.be>
2016-10-22 17:20:20 +0200

    TestTemplate addition to DatabaseLoader Currently initialised, but unused. No errors.

--- src/main/java/be/uantwerpen/fti/se/data/DatabaseLoader.java
+++ src/main/java/be/uantwerpen/fti/se/data/DatabaseLoader.java
@@ -2,11 +2,14 @@ package be.uantwerpen.fti.se.data;
 
 import be.uantwerpen.fti.se.model.Permission;
 import be.uantwerpen.fti.se.model.Role;
+import be.uantwerpen.fti.se.model.TestTemplate;
 import be.uantwerpen.fti.se.model.User;
 import be.uantwerpen.fti.se.repository.PermissionRepository;
 import be.uantwerpen.fti.se.repository.RoleRepository;
+import be.uantwerpen.fti.se.repository.TestTemplateRepository;
 import be.uantwerpen.fti.se.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -24,12 +27,14 @@ public class DatabaseLoader {
     private final PermissionRepository permissionRepository;
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
+    private final TestTemplateRepository testTemplateRepository;
 
     @Autowired
-    public DatabaseLoader(PermissionRepository permissionRepository, RoleRepository roleRepository, UserRepository userRepository) {
+    public DatabaseLoader(PermissionRepository permissionRepository, RoleRepository roleRepository, UserRepository userRepository, TestTemplateRepository testTemplateRepository) {
         this.permissionRepository = permissionRepository;
         this.roleRepository = roleRepository;
         this.userRepository = userRepository;
+        this.testTemplateRepository = testTemplateRepository;
     }
 
     @PostConstruct
@@ -39,20 +44,34 @@ public class DatabaseLoader {
         for (String p : allPermissions){
             permissionRepository.save(new Permission(p));
         }
+
+        //create logon permission and save it to the repository
         Permission p1 = new Permission("logon");
         permissionRepository.save(p1);
+
+        //create admin and tester roles
         Role administrator = new Role("Administrator");
         Role tester = new Role("Tester");
+
+        //add permission "logon" to the newly created list permissions.
         List<Permission> permissions =  new ArrayList<Permission>();
         permissions.add(p1);
+
+        //now add all permissions in permissions to the tester role (currently just 1)
         tester.setPermissions(permissions);
         roleRepository.save(tester);
+
+        //now add all permissions from the String array above (allPermissions) to the newly created permissions.
         permissions =  new ArrayList<Permission>();
         for (Permission p : permissionRepository.findAll()){
             permissions.add(p);
         }
+
+        //add all these permissions to the administrator role
         administrator.setPermissions(permissions);
         roleRepository.save(administrator);
+
+        //create users and set roles
         User u1 = new User("admin","admin");
         List<Role> roles = new ArrayList<>();
         roles.add(administrator);
@@ -64,5 +83,7 @@ public class DatabaseLoader {
         u2.setRoles(roles);
         userRepository.save(u2);
 
+        //test creating a template
+        TestTemplate t1 = new TestTemplate("Template_0x01", "This is the very first test template with no use whatsoever. Ignore it.", 0);
     }
 }

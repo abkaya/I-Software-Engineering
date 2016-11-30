956711a85441c4c8cdd134270e3001fd0d58a503
Abdil Kaya <abdil.kaya@student.uantwerpen.be>
2016-11-30 17:08:23 +0100

    Post-merge homepage and databaseloader conformity The reason for the merge from sprint3 to Report_TestObject was to bring in the changes to TestPlan, which brought the necessary ManyToOne  link from testplan to device.

--- src/main/java/be/uantwerpen/fti/se/data/DatabaseLoader.java
+++ src/main/java/be/uantwerpen/fti/se/data/DatabaseLoader.java
@@ -26,11 +26,9 @@ public class DatabaseLoader {
     private final TestSequenceRepository testSequenceRepository;
     private final DeviceRepository deviceRepository;
     private final TestPlanRepository testPlanRepository;
-    private final TestObjectRepository testObjectRepository;
-    private final ResultRepository resultRepository;
 
     @Autowired
-    public DatabaseLoader(PermissionRepository permissionRepository, RoleRepository roleRepository, UserRepository userRepository, TestTemplateRepository testTemplateRepository, TestTemplateService testTemplateService, TestSequenceRepository testSequenceRepository, DeviceRepository deviceRepository, TestPlanRepository testPlanRepository, TestObjectRepository testObjectRepository, ResultRepository resultRepository) {
+    public DatabaseLoader(PermissionRepository permissionRepository, RoleRepository roleRepository, UserRepository userRepository, TestTemplateRepository testTemplateRepository, TestTemplateService testTemplateService, TestSequenceRepository testSequenceRepository, DeviceRepository deviceRepository, TestPlanRepository testPlanRepository) {
         this.permissionRepository = permissionRepository;
         this.roleRepository = roleRepository;
         this.userRepository = userRepository;
@@ -39,8 +37,6 @@ public class DatabaseLoader {
         this.testSequenceRepository = testSequenceRepository;
         this.deviceRepository = deviceRepository;
         this.testPlanRepository = testPlanRepository;
-        this.testObjectRepository = testObjectRepository;
-        this.resultRepository = resultRepository;
     }
 
     @PostConstruct
@@ -89,12 +85,6 @@ public class DatabaseLoader {
         roles.add(tester);
         u2.setRoles(roles);
         userRepository.save(u2);
-        User u3 = new User("user2", "user2");
-        roles = new ArrayList<>();
-        roles.add(tester);
-        u3.setRoles(roles);
-        userRepository.save(u3);
-
 
         //create sequences and add these to the repository
         /*TestSequence ts0 = new TestSequence();
@@ -145,8 +135,8 @@ public class DatabaseLoader {
 
 
         //Add test plans
-        TestPlan tp1 = new TestPlan("Testplan_0x01","08/08","09/08","This is a description for testplan 1");
-        TestPlan tp2 = new TestPlan("Testplan_0x02","08/08","09/08","This is a description for testplan 2");
+        TestPlan tp1 = new TestPlan("Testplan_0x01","This is a description for testplan 1");
+        TestPlan tp2 = new TestPlan("Testplan_0x02","This is a description for testplan 2");
 
         //t1.setEditable(false);   //Add sequences to templates, templates to plans at runtime in order to keep things working.
         //tp1.setTestTemplate(t1);
@@ -159,7 +149,7 @@ public class DatabaseLoader {
         d1.setIsUsed();
         testPlanDevices.add(d1);
         d1.setIsInUse();
-        tp1.setDevices(testPlanDevices);
+        tp1.setDevice(d1);
         testPlanRepository.save(tp1);
 
         //t2.setEditable(false);
@@ -172,56 +162,8 @@ public class DatabaseLoader {
         d2.setIsInUse();
         d2.setIsUsed();
         testPlanDevices.add(d2);
-        tp2.setDevices(testPlanDevices);
+        tp2.setDevice(d2);
         testPlanRepository.save(tp2);
-
-
-        TestObject to1 = new TestObject("TestObject 1", t1.getId(), u2.getUserName(), tp1);
-        List<Result> resList1 = new ArrayList<>();
-        Result r1 = new Result(5,10,1000,2.53);
-        Result r2 = new Result(6,7,1200,5.6);
-        Result r3 = new Result(3,6.5,980,4);
-        Result r4 = new Result(4.3,8,1100,3.6);
-        resultRepository.save(r1);
-        resultRepository.save(r2);
-        resultRepository.save(r3);
-        resultRepository.save(r4);
-        List<Long> seqList1 = new ArrayList<>();
-        seqList1.add(t1.getTestSequences().get(0).getId());
-        seqList1.add(t1.getTestSequences().get(1).getId());
-        seqList1.add(t1.getTestSequences().get(2).getId());
-        seqList1.add(t1.getTestSequences().get(3).getId());
-        resList1.add(r1);
-        resList1.add(r2);
-        resList1.add(r3);
-        resList1.add(r4);
-        to1.setResults(resList1);
-        testObjectRepository.save(to1);
-
-
-
-        TestObject to2 = new TestObject("TestObject 2", t1.getId(), u3.getUserName(), tp1);
-        Result r5 = new Result(6,7,970,38);
-        Result r6 = new Result(3.1,6.23,1235,80.3);
-        Result r7 = new Result(1.8,9.5,1300,15.2);
-        Result r8 = new Result(4,6.8,650,20.9);
-        resultRepository.save(r5);
-        resultRepository.save(r6);
-        resultRepository.save(r7);
-        resultRepository.save(r8);
-        List<Long> seqList2 = new ArrayList<>();
-        seqList2.add(t1.getTestSequences().get(0).getId());
-        seqList2.add(t1.getTestSequences().get(1).getId());
-        seqList2.add(t1.getTestSequences().get(2).getId());
-        seqList2.add(t1.getTestSequences().get(3).getId());
-        to1.setSequences(seqList2);
-        List<Result> resList2 = new ArrayList<>();
-        resList2.add(r5);
-        resList2.add(r6);
-        resList2.add(r7);
-        resList2.add(r8);
-        to1.setResults(resList2);
-        testObjectRepository.save(to1);
 
 
     }
